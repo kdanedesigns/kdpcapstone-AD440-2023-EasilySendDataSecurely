9addfb0248c297657a9fd282345115828d8c6efc
Scott Hansford <zenwattage@gmail.com>
2023-01-25 18:29:10 -0800

    pushing working scaffold of basic encryption/decryption UI

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Decryption from './components/Decryption';
+import Encryption from './components/Encryption';
+
+
+// TODO: Split the components into seperate routes and use react - router - dom to navigate between them(https://reactrouter.com/web/guides/quick-start)
+// TODO: Upload the backend code to AWS Lambda and connect the frontend to it
+// TODO: Connect the components to AWS Lambda functions to encrypt and decrypt data
+
+
+function App() {
+    return (
+        <div className="App">
+            <Encryption />
+            <Decryption />
+        </div>
+    );
+}
+
+export default App;
