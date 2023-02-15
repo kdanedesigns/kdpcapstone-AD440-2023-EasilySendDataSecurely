14d47b8e1b2c6b845b428264b19cba50819ddc24
kennedy <kylekenne@gmail.com>
2023-02-15 11:32:11 -0800

    adding .test.jsx file, updated yaml

---
+++ src/components/Encryption.test.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Encryption from './Encryption';
+
+describe('Encryption', () => {
+  it('should render successfully', () => {
+    render(<Encryption />);
+    expect(screen.getByText('Encrypted Text:')).toBeTruthy();
+  });
+
+  it('should be able to encrypt text', () => {
+    render(<Encryption />);
+    // find the pages components
+    const textarea = screen.getByLabelText('Message');
+      const passwordInput =
+screen.getByLabelText('Password');
+    const encryptButton = screen.getByText('Encrypt');
+    // Type in the textarea and password
+    userEvent.type(textarea, 'Hello World');
+    userEvent.type(passwordInput, 'password');
+    // Click the encrypt button
+    encryptButton.click();
+    // Check that the encrypted text is displayed
+      expect(screen.getByText('Encrypted Text: ')).toBeTruthy();
+  });
+});
---
+++ src/components/EncryptionExample.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import CryptoJS from "crypto-js";
+function EncryptionExample() {
+  const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+  const [encryptedText, setEncryptedText] = useState("");
+  const handleEncryption = async () => {
+    const ciphertext = CryptoJS.AES.encrypt(text, password);
+    setEncryptedText(ciphertext.toString());
+    await navigator.clipboard.writeText(`${ciphertext.toString()}`);
+  };
+  return (
+    <>
+      <div className="encryptInputs">
+        <textarea
+          airaLabel="Message"
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          airaLabel="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleEncryption}>Encrypt</button>
+      </div>
+      <div className="output">
+        <p>Encrypted Text: {encryptedText}</p>
+      </div>
+    </>
+  );
+}
+export default EncryptionExample;
