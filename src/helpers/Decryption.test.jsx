e3db48865294501955154735311b3158122311f4
kennedy <kylekenne@gmail.com>
2023-02-16 13:44:38 -0800

    adding helpers folder to /src

---
+++ src/helpers/Decryption.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Decryption from '../components/Decryption';
+
+describe('Decryption', () => {
+  it('should render successfully', () => {
+    render(<Decryption />);
+    expect(screen.getByText('Decrypted Text:')).toBeTruthy();
+  });
+
+  it('', () => {
+    render(<Decryption />);
+    // find the pages components
+    const textarea = screen.getByLabelText('text');
+      const passwordInput = screen.getByLabelText( 'password' );
+      const decryptButton = screen.getByText( 'Decrypt' );
+    // Type in the textarea and password
+      userEvent.type( textarea, 'text' );
+      userEvent.type( passwordInput, 'password' );
+    // Click the encrypt button
+      decryptButton.click();
+    // Check that the encrypted text is displayed
+      expect( screen.getByText( 'Decrypted Text: ' ) ).toBeTruthy();
+  } );
+} );
