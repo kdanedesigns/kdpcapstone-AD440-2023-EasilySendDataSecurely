d1776d893dc645821dd5c8f6dd4ed761431f197b
1jc <34551673+1jc@users.noreply.github.com>
2023-02-21 00:05:34 -0800

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HeaderExample() {
+
+    return (
+        <div className='headerContainer'>        
+        <h1>Encryption/Decryption Website</h1>
+        <p>Key in the encryption key in the encryption field</p>
+        <p>Key in your plain text or encrypted data in the data field</p>
+        <p>Select the encrypt key to encrypt your data or the decrypt key to decrypt your data, </p>
+        </div>
+    );
+}
+
+
+export default HeaderExample;
