3818f2581857e21c50bf839d7c411cc9435bf58c
1jc <34551673+1jc@users.noreply.github.com>
2023-02-22 09:54:55 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Decryption from './components/Decryption';
 import Encryption from './components/Encryption';
-
+import Header from './components/Header';    //1jc added for changes to UI
 
 function App() {
     return (
         <div className="App">
+        <Header/>                                       //1jc added for changes to UI
+        <div className="encryptDecryptContainer1">      //1jc added for changes to UI
             <Encryption />
             <Decryption />
         </div>
+       </div>
     );
 }
 
