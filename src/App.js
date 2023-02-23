7f33a7fe767d914db23636c948fc2122c41a83d3
1jc <34551673+1jc@users.noreply.github.com>
2023-02-23 14:32:28 -0800

    1jc update App.js reference to pull #149 which needed div for container1 in App.css
    1jc update App.js reference to pull #149  which needed div for container1 in App.css
    #122

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import Decryption from './components/Decryption';
 import Encryption from './components/Encryption';
-import Header from './components/Header';    //1jc added for changes to UI
+import Header from './components/Header';    
 
 function App() {
     return (
         <div className="App">
-        <Header/>                                       //1jc added for changes to UI
-                                                        //1jc took out line 11 per review of reviewer
+        <Header/>                                       
+         <div className="encryptDecryptContainer1">                                               
             <Encryption />
             <Decryption />
         </div>
-                                                        //1jc took out line 15 per review of reviewer
+     </div>                                                   
     );
 }
 
