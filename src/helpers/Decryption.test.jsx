40a31b591d7e09402da116997950d805897f1fd7
kennedy <kylekenne@gmail.com>
2023-02-23 16:31:45 -0800

    troubleshooting errors

--- src/helpers/Decryption.test.jsx
+++ src/helpers/Decryption.test.jsx
@@ -5,28 +5,24 @@ import userEvent from '@testing-library/user-event';
 import Decryption from '../components/Decryption';
 
 describe('Decryption', () => {
-
   it('should render successfully', async () => {
     render(<Decryption />);
     expect(screen.getByText('Decrypted Text:')).toBeTruthy();
     await waitFor(() => {
       expect(screen.getByText('Decrypted Text:')).toBeTruthy();
     });
-
+  } );
+  
   it('should be able to decrypt text', () => {
     render(<Decryption />);
-    // find the pages components
     const textarea = screen.getByLabelText('text');
     const passwordInput = screen.getByLabelText( 'password' );
 screen.getByLabelText('password');
     const decryptButton = screen.getByText('Decrypt');
-    // Type in the textarea and password
     userEvent.type(textarea, 'text');
     userEvent.type(passwordInput, 'password');
-    // Click the encrypt button
     decryptButton.click();
-    // Check that the encrypted text is displayed
     expect(screen.getByText('Decrypted Text: ')).toBeTruthy();
   });
-  } );
 });
+
--- src/helpers/Encryption.test.jsx
+++ src/helpers/Encryption.test.jsx
@@ -7,26 +7,22 @@ import Encryption from '../components/Encryption';
 describe('Encryption', () => {
   it('should render successfully', async () => {
     render(<Encryption />);
-    expect( screen.getByText( 'Encrypted Text:' ) ).toBeTruthy();
+    expect( screen.getByText('Encrypted Text:')).toBeTruthy();
     await waitFor( () => { 
-      expect( screen.getByText( 'Eecrypted Text:' ) ).toBeTruthy();
-    } );
-  } );
+      expect( screen.getByText('Encrypted Text:')).toBeTruthy();
+    });
+  });
   
   it('should be able to encrypt text', () => {
     render(<Encryption />);
-    // find the pages components
     const textarea = screen.getByLabelText('text');
     const passwordInput = screen.getByLabelText('password');
 screen.getByLabelText('password');
 
     const encryptButton = screen.getByText('Encrypt');
-    // Type in the textarea and password
     userEvent.type(textarea, 'text');
     userEvent.type(passwordInput, 'password');
-    // Click the encrypt button
     encryptButton.click();
-    // Check that the encrypted text is displayed
     expect(screen.getByText('Encrypted Text: ')).toBeTruthy();
   });
 });
