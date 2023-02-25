8eabca72ea28d9c2f0e50165c2abfb69b64ddea0
Kyle Bastien <Kyle.Bastien@docusign.com>
2023-02-24 18:36:55 -0800

    Fix tests

--- src/helpers/Encryption.test.jsx
+++ src/helpers/Encryption.test.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Encryption from '../components/Encryption';
+import Encryption from "../components/Encryption";
 
-describe('Encryption', () => {
-  it('should render successfully', () => {
+describe("Encryption", () => {
+  it("should render successfully", () => {
     render(<Encryption />);
-    expect(screen.getByText('Encrypted Text:')).toBeTruthy();
+    expect(screen.getByText("Encrypted Text:")).toBeTruthy();
   });
 
-  it('should be able to encrypt text', () => {
+  it("should be able to encrypt text", () => {
     render(<Encryption />);
     // find the pages components
-    const textarea = screen.getByLabelText('text');
-    const passwordInput = screen.getByLabelText('password');
-    const encryptButton = screen.getByText('Encrypt');
+    const textarea = screen.getByLabelText("text");
+    const passwordInput = screen.getByLabelText("password");
+    const encryptButton = screen.getByText("Click to Encrypt");
     // Type in the textarea and password
-    userEvent.type(textarea, 'text');
-    userEvent.type(passwordInput, 'password');
+    userEvent.type(textarea, "text");
+    userEvent.type(passwordInput, "password");
     // Click the encrypt button
     encryptButton.click();
     // Check that the encrypted text is displayed
-    expect(screen.getByText('Encrypted Text: ')).toBeTruthy();
+    expect(screen.getByText("Encrypted Text:")).toBeTruthy();
   });
 });
