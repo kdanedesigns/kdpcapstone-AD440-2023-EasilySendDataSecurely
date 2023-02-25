e6d90478eb66d39f02fe24984232e36f6390ac9e
Kyle Bastien <Kyle.Bastien@docusign.com>
2023-02-24 18:24:03 -0800

    Revert "Merge branch 'main' into damo_appcss"
    This reverts commit 298a0df8b6519f48356cbfa1d4e93e67472d0d05, reversing
    changes made to 03d09111da98841b46aa19d6a386fbe89208e6f9.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import './App.css';
 import Decryption from './components/Decryption';
 import Encryption from './components/Encryption';
-
-import Header from './components/Header';
+import Header from './components/Header';    //1jc added for changes to UI
 
 function App() {
     return (
-        <div className="App">   
-            <Header/>
-            <div className="encryptDecryptContainer1">   
-                <Encryption />
-                <Decryption />
-            </div>
+        <div className="App">
+        <Header/>                                       //1jc added for changes to UI
+                                                        //1jc took out line 11 per review of reviewer
+            <Encryption />
+            <Decryption />
         </div>
+                                                        //1jc took out line 15 per review of reviewer
     );
 }
 
