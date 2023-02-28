01df533eeeeef028537bec7615af6638bee59b41
Scott Hansford <zenwattage@gmail.com>
2023-02-28 12:29:05 -0800

    clearing inputs after encrypt/decrypt, added empty key error alert for decryption.js

--- src/components/Decryption.js
+++ src/components/Decryption.js
@@ -2,82 +2,92 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const DecryptionComponent = () => {
-  const [text, setText] = useState("");
-  // const [encryptedText, setEncryptedText] = useState("");
-  const [password, setPassword] = useState("");
-  const [decryptedText, setDecryptedText] = useState("");
-  const [decrypted, setDecrypted] = useState(false);
+    const [text, setText] = useState("");
+    // const [encryptedText, setEncryptedText] = useState("");
+    const [password, setPassword] = useState("");
+    const [decryptedText, setDecryptedText] = useState("");
+    const [decrypted, setDecrypted] = useState(false);
+    // const [errorMessage, setErrorMessage] = useState("");
+    const handleDecryption = async () => {
+        try {
 
-  const handleDecryption = async () => {
-    try {
-      axios
-        .post(
-          "https://fogbnvtkba.execute-api.us-west-2.amazonaws.com/Decrypt-AD440Winter2023-V3",
-          { encryptedMessage: text, key: password }
-        )
+            axios
+                .post(
+                    "https://fogbnvtkba.execute-api.us-west-2.amazonaws.com/Decrypt-AD440Winter2023-V3",
+                    { encryptedMessage: text, key: password }
+                )
 
-        .then(function (response) {
-          console.log(response.data);
-          // console.log(response.data.key);
-          // original code setEncryptedText(response.data);
-          setDecryptedText(response.data.decryptedMessage);
-        })
-        .catch(function (error) {
-          if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            console.log(error.request);
-          } else {
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
-      setDecrypted(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return (
-    <>
-      <div className="encryptDecryptContainer2">
-        <div className="encryptDecryptContainer3">
-          <p>key</p>
-          <input
-            aria-label="password"
-            className="encryptDecryptContainer3a"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+                .then(function (response) {
+                    if (password == "") {
+                        alert("Invalid Key or No Key Provided")
+                        // setDecryptedText("Invalid Key or No Key Provided");
+                    }
+                    setDecryptedText(response.data.decryptedMessage);
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                        setDecryptedText(error.response.data);
+                    } else if (error.request) {
+                        // The request was made but no response was received
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                });
+            setDecrypted(true);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
+        <>
+            <div className="encryptDecryptContainer2">
+                <div className="encryptDecryptContainer3">
+                    <p>key</p>
+                    <input
+                        aria-label="password"
+                        className="encryptDecryptContainer3a"
+                        placeholder="Input Decryption Key Here: "
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        //clear password box after decryption
+                        value={decrypted ? "" : password}
+                    />
+                </div>
 
-        <div className="encryptDecryptContainer3b">
-          <p>text</p>
-          <textarea
-            aria-label="text"
-            className="encryptDecryptContainer3a"
-            type="text"
-            onChange={(e) => setText(e.target.value)}
-          ></textarea>
-        </div>
+                <div className="encryptDecryptContainer3b">
+                    <p>text</p>
+                    <textarea
+                        aria-label="text"
+                        className="encryptDecryptContainer3a"
+                        placeholder="Input Encrypted Text Here: "
+                        type="text"
+                        onChange={(e) => setText(e.target.value)}
+                        //clear text box after decryption
+                        value={decrypted ? "" : text}
+                    ></textarea>
+                </div>
 
-        <div className="encryptDecryptContainer4">
-          <button
-            className="encryptDecryptContainer4b"
-            onClick={handleDecryption}
-          >
-            Decrypt
-          </button>
-        </div>
+                <div className="encryptDecryptContainer4">
+                    <button
+                        className="encryptDecryptContainer4b"
+                        onClick={handleDecryption}
+                    >
+                        {decrypted ? 'Decrypted!' : 'Click to Decrypt'}
+                    </button>
+                </div>
 
-        <div className="encryptDecryptContainer3c">
-          <p>Decrypted Text: {decryptedText}</p>
-        </div>
-      </div>
-    </>
-  );
+                <div className="encryptDecryptContainer3c">
+                    <p>Decrypted Text: {decryptedText}</p>
+
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default DecryptionComponent;
--- src/components/Encryption.js
+++ src/components/Encryption.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function EncryptionExample() {
-  const [text, setText] = useState("");
-  const [password, setPassword] = useState("");
-  const [encryptedText, setEncryptedText] = useState("");
+  const [text, setText] = useState('');
+  const [password, setPassword] = useState('');
+  const [encryptedText, setEncryptedText] = useState('');
 
   const [encrypted, setEncrypted] = useState(false);
 
   const handleEncryption = async () => {
+
     try {
+      if (password == '') {
+        setEncryptedText('Invalid Key');
+      }
+
       axios
         .post(
-          "https://fogbnvtkba.execute-api.us-west-2.amazonaws.com/Encrypt-AD440Winter2023-V3",
+          'https://fogbnvtkba.execute-api.us-west-2.amazonaws.com/Encrypt-AD440Winter2023-V3',
           { message: text, key: password }
         )
         .then(function (response) {
           setEncryptedText(response.data.encryptedMessage);
+
         })
         .catch(function (error) {
           if (error.response) {
@@ -27,32 +33,54 @@ function EncryptionExample() {
             // The request was made but no response was received
             console.log(error.request);
           } else {
-            console.log("Error", error.message);
+            console.log('Error', error.message);
           }
           console.log(error.config);
         });
       setEncrypted(true);
-      await navigator.clipboard.writeText(`${encryptedText}`);
     } catch (error) {
       console.error(error);
     }
+    //TODO: FIX COPY FUNCTIONALITY: CURRENTLY REQUIRES TWO CLICKS TO COPY
+    await navigator.clipboard.writeText(`${encryptedText}`);
   };
 
   return (
     <>
-      <div className='encryptDecryptContainer2'>
+      <div className="encryptDecryptContainer2">
         <div className="encryptDecryptContainer3">
           <p>key</p>
-          <input aria-label="password" className="encryptDecryptContainer3a" type="password" onChange={e => setPassword(e.target.value)} />
+          <input
+            aria-label="password"
+            className="encryptDecryptContainer3a"
+            type="password"
+            placeholder="Input Encryption Key Here: "
+            onChange={(e) => setPassword(e.target.value)}
+            //clear password box after encryption
+            value={encrypted ? '' : password}
+          />
         </div>
         <div className="encryptDecryptContainer3b">
           <p>text</p>
-          <textarea aria-label="text" className="encryptDecryptContainer3a" type="text" onChange={e => setText(e.target.value)} />
+          <textarea
+            aria-label="text"
+            className="encryptDecryptContainer3a"
+            placeholder="Input Text to Encrypt Here: "
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            //clear text box after encryption
+            value={encrypted ? '' : text}
+          />
         </div>
         <div className="encryptDecryptContainer4">
-          <button className="encryptDecryptContainer4a" onClick={handleEncryption}>{encrypted ? "Encrypted!" : "Click to Encrypt"}</button>
+          <button
+            className="encryptDecryptContainer4a"
+            onClick={handleEncryption}
+          >
+            {encrypted ? 'Encrypted!' : 'Click to Encrypt'}
+          </button>
         </div>
-        <div className='encryptDecryptContainer3c'>
+        <div className="encryptDecryptContainer3c">
           <p>Encrypted Text: {encryptedText}</p>
         </div>
       </div>
