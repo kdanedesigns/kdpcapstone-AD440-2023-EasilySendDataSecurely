9824d32b0fc3d346efea74c01af872f8b22c9563
Scott Hansford <zenwattage@gmail.com>
2023-02-27 17:03:39 -0800

    updating encryption comp

--- src/components/Encryption.js
+++ src/components/Encryption.js
@@ -40,27 +40,23 @@ function EncryptionExample() {
 
   return (
     <>
-      <div className="encryptInputs">
-        <textarea
-          aria-label="text"
-          type="text"
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input
-          aria-label="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button onClick={handleEncryption}>
-          {encrypted ? "Encrypted!" : "Click to Encrypt"}
-        </button>
-      </div>
-      <div className="output">
-        <p>Encrypted Text: {encryptedText}</p>
+      <div className='encryptDecryptContainer2'>
+        <div className="encryptDecryptContainer3">
+          <p>key</p>
+          <input className="encryptDecryptContainer3a" type="password" onChange={e => setPassword(e.target.value)} />
+        </div>
+        <div className="encryptDecryptContainer3b">
+          <p>text</p>
+          <textarea className="encryptDecryptContainer3a" type="text" onChange={e => setText(e.target.value)} />
+        </div>
+        <div className="encryptDecryptContainer4">
+          <button className="encryptDecryptContainer4a" onClick={handleEncryption}>Encrypt</button>
+        </div>
+        <div className='encryptDecryptContainer3c'>
+          <p>Encrypted Text: {encryptedText}</p>
+        </div>
       </div>
     </>
   );
 }
-
 export default EncryptionExample;
