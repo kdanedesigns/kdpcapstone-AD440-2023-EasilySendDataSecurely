6201a8d6df32df9995777fba21d1a839a871990a
Scott Hansford <zenwattage@gmail.com>
2023-03-01 12:45:01 -0800

    added updated button test to the decrytButton in decryption.test.jsx

--- src/helpers/Decryption.test.jsx
+++ src/helpers/Decryption.test.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import Decryption from "../components/Decryption";
+import Decryption from '../components/Decryption';
 
-describe("Decryption", () => {
-  it("should render successfully", () => {
+describe('Decryption', () => {
+  it('should render successfully', () => {
     render(<Decryption />);
-    expect(screen.getByText("Decrypted Text:")).toBeTruthy();
+    expect(screen.getByText('Decrypted Text:')).toBeTruthy();
   });
 
-  it("should be able to decrypt text", () => {
+  it('should be able to decrypt text', () => {
     render(<Decryption />);
     // find the pages components
-    const textarea = screen.getByLabelText("text");
-    const passwordInput = screen.getByLabelText("password");
-    const decryptButton = screen.getByText("Decrypt");
+    const textarea = screen.getByLabelText('text');
+    const passwordInput = screen.getByLabelText('password');
+    const decryptButton = screen.getByText('Click to Decrypt');
     // Type in the textarea and password
-    userEvent.type(textarea, "Hello Decypted");
-    userEvent.type(passwordInput, "Password");
+    userEvent.type(textarea, 'Hello Decypted');
+    userEvent.type(passwordInput, 'Password');
     // Click the encrypt button
     decryptButton.click();
     // Check that the encrypted text is displayed
-    expect(screen.getByText("Decrypted Text:")).toBeTruthy();
+    expect(screen.getByText('Decrypted Text:')).toBeTruthy();
   });
 });
