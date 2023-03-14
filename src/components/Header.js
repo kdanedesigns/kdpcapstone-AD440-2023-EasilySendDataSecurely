5a87f12ae951103f492bd846746c8ecec2a53372
Scott Hansford <zenwattage@gmail.com>
2023-03-13 22:11:11 -0700

    pushing copy/paste button addition, overhaul of css, adding react-ico… (#193)
    * pushing copy/paste button addition, overhaul of css, adding react-icons & react-boostrap
    
    * adding updated App.js
    
    * pushing updated package.json
    
    * updating package-lock.json
    
    * adding app.css index.css header.js
    
    * adding app.css and index.css AGAIN
    
    * adding bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import logo from "../images/lightlogo.png"
+import "./header.css";
 
-import encryptDecryptPic from "../images/encryptDecryptZaya.jpg"; // gives image path
-import logo from "../images/logoScott.png"; // gives image path
-import logoOption13 from "../images/Logo_Option_13.jpg"; // gives image path
-import logoOption14 from "../images/Logo_Option_14.jpg"; // gives image path
 function HeaderExample() {
   return (
-    <div className="logoAndBackground">
-      <img
-        src={logoOption14}
-        width={250}
-        height={300}
-        alt="this is the group logo"
-      />
-      <div className="headerContainer">
-        <h1>Encryption/Decryption Website</h1>
-        <p>Key in the encryption key in the encryption field</p>
-        <p>Key in your plain text or encrypted data in the data field</p>
-        <p>
-          Select the encrypt key to encrypt your data or the decrypt key to
-          decrypt your data,{" "}
-        </p>
-      </div>
-      <img
-        src={encryptDecryptPic}
-        width={400}
-        height={175}
-        alt="this is Zaya's encryptdecrypt background jpg the group logo"
-      />
-    </div>
+    <Container className="logoContainer" >
+      <Row>
+        <Col>
+          <img
+            className="logo"
+            src={logo}
+            alt="application logo"
+          />
+        </Col>
+      </Row>
+    </ Container>
   );
 }
 export default HeaderExample;
